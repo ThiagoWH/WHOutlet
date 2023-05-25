5910aeb075fed6b4a1fba581b363ba18c6af01c7
David <davidlimait@outlook.com>
2023-05-25 20:47:17 -0300

    Inserido o Footer na App Vue

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-background">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">
         <img src="../img/Logo.png" alt="" width="30px" height="30px" class="d-inline-block align-text-top">
@@ -24,3 +24,8 @@
     </div>
 </nav>
 </template>
+<style>
+.navbar-background {
+  background-color: #1c2331;
+}
+</style>
