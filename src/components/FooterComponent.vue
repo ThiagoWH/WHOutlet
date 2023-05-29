b765a790509d4144b12099a0ca86a18842cb3dd7
ThiagoWH <thiagodominguesj@gmail.com>
2023-05-28 21:00:18 -0300

    foorter alterado

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,7 +5,7 @@
 <footer class="text-center text-lg-start text-white footer">
   <!-- Section: Social media -->
   <section
-           class="d-flex justify-content-between p-4 social-login">
+          class="d-flex justify-content-between p-4 social-login">
     <!-- Left -->
     <div class="me-5">
       <span>Conecte-se conosco nas redes sociais:</span>
@@ -17,15 +17,12 @@
       <a href="" class="text-white me-4">
         <i class="fab fa-facebook-f"></i>
       </a>
-      <a href="" class="text-white me-4">
+      <a href="https://www.instagram.com/wh_outlet/" class="text-white me-4" target="_blank" rel="noopener noreferrer">
         <i class="fab fa-instagram">Instagram</i>
       </a>
-      <a href="" class="text-white me-4">
-        <i class="fab fa-linkedin">Linkedin</i>
-      </a>
-      <a href="" class="text-white me-4">
-        <i class="fab fa-github">GitHub</i>
-      </a>
+      <a href="https://api.whatsapp.com/send/?phone=5511977899540&text&type=phone_number&app_absent=0" class="text-white me-4" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-instagram">WhatsApp</i>
+        </a>
     </div>
     <!-- Right -->
   </section>
@@ -55,45 +52,19 @@
         <!-- Grid column -->
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold">Products</h6>
+          <h6 class="text-uppercase fw-bold">Produtos</h6>
           <hr
               class="mb-4 mt-0 d-inline-block mx-auto"
               style="width: 60px; background-color: #7c4dff; height: 2px"
               />
           <p>
-            <a href="#!" class="text-white">MDBootstrap</a>
+            <a href="/Masculino" class="text-white">Masculino</a>
           </p>
           <p>
-            <a href="#!" class="text-white">MDWordPress</a>
+            <a href="/Feminino" class="text-white">Feminino</a>
           </p>
           <p>
-            <a href="#!" class="text-white">BrandFlow</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">Bootstrap Angular</a>
-          </p>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold">Useful links</h6>
-          <hr
-              class="mb-4 mt-0 d-inline-block mx-auto"
-              style="width: 60px; background-color: #7c4dff; height: 2px"
-              />
-          <p>
-            <a href="#!" class="text-white">Your Account</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">Become an Affiliate</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">Shipping Rates</a>
-          </p>
-          <p>
-            <a href="#!" class="text-white">Help</a>
+            <a href="/selecoes" class="text-white">Seleções</a>
           </p>
         </div>
         <!-- Grid column -->
@@ -101,15 +72,14 @@
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold">Contact</h6>
+          <h6 class="text-uppercase fw-bold">Contato</h6>
           <hr
               class="mb-4 mt-0 d-inline-block mx-auto"
               style="width: 60px; background-color: #7c4dff; height: 2px"
               />
-          <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-          <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-          <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          <p><i class="fas fa-home mr-3"></i> São Paulo, SP, BR</p>
+          <p><i class="fas fa-envelope mr-3"></i> contato@whoutlet.com</p>
+          <p><i class="fas fa-phone mr-3"></i> + 55 11 97789-9540</p>
         </div>
         <!-- Grid column -->
       </div>
@@ -120,13 +90,13 @@
 
   <!-- Copyright -->
   <div
-       class="text-center p-3"
-       style="background-color: rgba(0, 0, 0, 0.2)"
-       >
-    © 2023 Copyright:
-    <a class="text-white" href="#"
-       >Wh Outleet</a
+    class="text-center p-3"
+      style="background-color: rgba(0, 0, 0, 0.2)"
       >
+  © 2023 Copyright:
+  <a class="text-white" href="/"
+      >Wh Outlet</a
+    >
   </div>
   <!-- Copyright -->
 </footer>
---
+++ src/views/PaginaDeSucessoView.vue
@@ -0,0 +1,3 @@
+<template>
+        <img src="../img/compraRealizada.gif" alt="">
+</template>
